4da8c3996998446484232f5a9c0055082390ca27
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-10-10 00:39:46 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,114 @@
+//  1-masala
+// let bugatti = ["shiron", "veyron", "tourbillion"];
+// console.log(bugatti[0]);
+// console.log(bugatti[2]);
+
+// 2-masala
+// let bugatti = ["shiron", "veyron", "tourbillion", "Centodieci", "xayr"];
+// bugatti[4] = "salom";
+// console.log(bugatti);
+
+// 3-masala
+// let fruits = [];
+// fruits.push("olma");
+// fruits.push("anjir");
+// fruits.push("anor");
+// console.log(fruits);
+
+// 4 - masala;
+// let bugatti = ["shiron", "veyron", "tourbillion", "Centodieci", "xayr"];
+// console.log(bugatti.length);
+
+// 5-masala
+// let bugatti = ["shiron", "veyron", "tourbillion", "Centodieci", "xayr"];
+// bugatti.pop("");
+// console.log(bugatti);
+
+// 6-masala
+// let bugatti = ["shiron", "veyron", "tourbillion", "Centodieci", "xayr"];
+// bugatti.unshift("damas");
+// console.log(bugatti);
+
+// 7-masala
+// let bugatti = ["shiron", "veyron", "tourbillion", "Centodieci", "xayr"];
+// bugatti.shift("");
+// console.log(bugatti);
+
+// 8-masala
+// let bugatti = ["shiron", "veyron", "tourbillion", "Centodieci", "xayr"];
+// console.log(bugatti);
+
+// 9-masala
+// 9ni tushunmadim va bu yerda xato ketti, hammasi bolib 8 masala ishladim, 1 tasni tushunmadim, 9 ta masala bolyapti lekin 1 ta masala qaysi birini yechmaganimni tushunmayapman.
+
+// For loop
+
+// 1-masala
+
+// let bugatti = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < bugatti.length; i++) {
+//   console.log(bugatti[i]);
+// }
+
+// 2-masala
+// let bugatti = ["shiron", "veyron", "tourbillion", "Centodieci", "xayr"];
+// for (let i = 0; i < bugatti.length; i++) {
+//   console.log(bugatti[i]);
+// }
+// buni meva dep nomlash shart emas menimcha chunki mashinar ham stringda ""
+
+// 3-masala
+
+// let bugatti = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < bugatti.length; i += 2) {
+//   console.log(bugatti[i]);
+// }
+
+// 4-masala
+// let bugatti = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < bugatti.length; i++) {
+//   if (bugatti[i] % 2 === 0) {
+//     console.log(bugatti[i]);
+//   }
+// }
+
+// 5-masala
+// let num = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
+// for (let i = 0; i < num.length; i++) {
+//   if (num[i] > 0) {
+//     console.log(num[i]);
+//   }
+// }
+
+// 6-masala
+// tushunaolmadim
+
+// 7-masala
+// let num = [1, 2, 3, 4, 5];
+// let count = 0;
+// for (let i = 0; i < num.length; i++) {
+//   count += num[i];
+// }
+// console.log(count);
+
+// 8-masala
+// tushunmadim
+
+// 9-masala
+// let num = [1, 2, 3, 4, 5];
+// let count = Math.max();
+// console.log(count);
+
+// 10 - masala
+// qiyin
+
+// 11 - masala
+
+// let num = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
+// for (let i = 0; i < num.length; i++) {
+//   if (num[i] < 0) {
+//     console.log(num[i]);
+//   }
+// }
+
+// qolganlari qiyin ekan 
